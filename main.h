ea64e02ec6bb65f7bc38283914e4bcbd8b93c20c
marcus022 <marcuschofi@lv>
2022-07-13 16:40:52 -0700

    hot fix

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+<<<<<<< HEAD
 int _putchar(char c);
 void reset_to_98(int *n);
+=======
+int _putchar(char c)
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+>>>>>>> hot fix
 
 #endif
