fcecc3f2e34356767264c14daec99312704a0636
marcus022 <marcuschofi@lv>
2022-07-13 18:00:13 -0700

    hot fix

--- 0-strcat.c
+++ 0-strcat.c
@@ -6,12 +6,13 @@
 * @src: second param
 * Return: a string
 */
-char *_strcat(char *dest, char *src)
+char *-strcat(char *dest, char *src)
 {
 	int len = 0, i;
 
 	while (dest[len])
 		len++;
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[len] = src[i];
@@ -19,3 +20,4 @@ char *_strcat(char *dest, char *src)
 	}
 	dest[len] = '\0';
 	return (dest);
+
